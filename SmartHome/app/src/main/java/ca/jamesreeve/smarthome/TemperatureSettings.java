9fdc3cf21806ccd1d5eeafbdf58de1f341c43224
RahulBablani <rahulbablani@hotmail.com>
2017-04-05 12:14:24 -0400

    added temp click fucntion

---
+++ SmartHome/app/src/main/java/ca/jamesreeve/smarthome/TemperatureSettings.java
@@ -0,0 +1,13 @@
+package ca.jamesreeve.smarthome;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TemperatureSettings extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_temperature_settings);
+    }
+}
